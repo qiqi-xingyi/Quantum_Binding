336743dce746d2bd3698a30534ec07f0af4857c5
yuqizhang <yuqizhang247@gmail.com>
2025-05-23 10:39:03 -0400

    #8055 error fix.

--- benchwork.py
+++ benchwork.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     # SCF + active space selection + problem & ansatz construction
     selector  = ActiveSpaceSelector(
         freeze_occ_threshold=1.98,
-        n_before_homo=1,
-        n_after_lumo=1
+        n_before_homo=2,
+        n_after_lumo=2
     )
     qp_builder = QiskitProblemBuilder(
         basis=basis,
